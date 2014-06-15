f44bc394ded52a3df4bf253b4aefa5b00fec7d98
Cristian Sepulveda <air.enrill@gmail.com>
2014-06-15 17:53:33 -0400

    se cambia orden mostrar categorias

--- stories/context_processors.py
+++ stories/context_processors.py
@@ -2,5 +2,5 @@ from .models import Category
 
 def category_list(request):
 	return {
-		'category_list': Category.objects.visible_parent()
+		'category_list': Category.objects.visible_parent().order_by('order')
 	}
