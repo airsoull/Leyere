3ca969aabc7f158b75d32be2e7fc31385e05bb03
Cristian Sepulveda <air.enrill@gmail.com>
2014-06-29 16:47:43 -0400

    cambio en admin profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,9 @@ from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = ('user',)
+	list_display = ('user', 'email_favorite_story', 'email_comment',)
 	readonly_fields = ('user',)
+	list_filter = ('email_favorite_story', 'email_comment',)
 
 	def has_add_permission(self, request):
 		return False
